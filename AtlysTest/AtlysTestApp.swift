549f5494c49b95e03a46c5b1fdff6ac1d2372f52
Piyush Sharma <Piyush.sharma@blu-smart.com>
2025-05-11 01:24:16 +0530

    Initial Commit

---
+++ AtlysTest/AtlysTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlysTestApp.swift
+//  AtlysTest
+//
+//  Created by Piyush on 11/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AtlysTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
